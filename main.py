e74edef0d776d18c188fc72b37aaaff9f33915ac
ftanveer <farhanxperia@gmail.com>
2022-08-30 23:33:01 -0400

    removed def hello

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ import io
 
 app = Flask(__name__)
 
-@app.route('/classify_image', methods = ["GET"])
+# @app.route('/classify_image', methods = ["GET"])
+#
+# def hello_world():
+#     return render_template('index.html')
 
-def hello_world():
-    return render_template('index.html')
 
-
-@app.route('/classify_image', methods = ["POST"])
+@app.route('/classify_image', methods = ["GET","POST"])
 
 # def base64ToString(a):
 #      return base64.b64decode(a).decode('utf-8')
